a6e6ad9cdd3c5520f5180a1dbe488ceab03c5f8b
Randy canela <randycanela02@gmail.com>
2020-06-28 11:56:14 -0400

    docs agregagdo

---
+++ docs/precache-manifest.96efdfa1d4a27c325181aa68caafc730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3e7b191e91848d228a1aebcafba313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b544c33476cbe213d6d5",
+    "url": "/static/css/main.fcb2468e.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b544c33476cbe213d6d5",
+    "url": "/static/js/main.e126f1b3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
